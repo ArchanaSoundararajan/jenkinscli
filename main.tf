b2e98f516fcf2501894ff9470c6a857933f7ba2a
ArchanaSoundararajan <79893793+ArchanaSoundararajan@users.noreply.github.com>
2023-10-30 17:49:03 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+
+terraform {
+  cloud {
+    hostname = "ck-tfe.sandpedia.com"
+    organization = "cloud-kinetics-admin"
+
+    workspaces {
+      name = "tfe-jenkins-cli"
+    }
+  }
+}
+resource "random_string" "random" {
+  length           = 8
+  special          = true
+  override_special = "/@£$"
+}
+
+output "result" {
+value = random_string.random.result
+}
+  
